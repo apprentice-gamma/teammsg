01390672114dd014121ca0381b716e42cc2e3326
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-04-15 10:44:04 -0400

    Do the IIFE shuffle in the dictator list controller, too...

--- app/js/ctrl/dictatorList.ctrl.js
+++ app/js/ctrl/dictatorList.ctrl.js
@@ -1,7 +1,11 @@
-angular
-	.module('dictatorTinder')
-	.controller('dictatorListCtrl', ['DictatorService', function(DictatorService) {
-	    var self = this;
-	    self.dictators = [];
-	    self.dictators = DictatorService.query();
-	  }]);
+(function(){
+	angular
+		.module('dictatorTinder')
+		.controller('dictatorListCtrl', DictatorListCtrl);
+
+		function DictatorListCtrl(DictatorService){
+			var self = this;
+		    self.dictators = [];
+		    self.dictators = DictatorService.query();
+		}
+});
--- app/js/ctrl/dictatorProfile.ctrl.js
+++ app/js/ctrl/dictatorProfile.ctrl.js
@@ -1,4 +1,5 @@
 (function(){}
+    
     angular
         .module('dictatorTinder')
         .controller('dictatorProfileCtrl', DictatorProfileCtrl);
