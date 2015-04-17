146bde30fdf8480aa2c5893657e5d2c9dd68f2bc
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-04-17 11:49:15 -0400

    Adds logic, does not display but also does not break anything significantly.

--- app/js/ctrl/controllers.js
+++ app/js/ctrl/controllers.js
@@ -5,6 +5,7 @@
     var self = this;
     self.dictators = [];
     self.dictators = DictatorService.query();
+    }
   }]);
 
   dictatorControllers.controller('dictatorProfileCtrl', ['DictatorService', '$routeParams', 'Bios', function(DictatorService, $routeParams, Bios) {
@@ -12,6 +13,20 @@
     self.dictator = DictatorService.get({id: $routeParams.dictatorId});
     self.randomBio = Bios[Math.floor(Math.random() * Bios.length)];
     self.randomNum = Math.floor(Math.random() * 22) + 1;
+    self.totalProles = function(){
+      var count = 0;
+      for (i=0; i < dictator.owned_households.length; i++)
+        count+= dictator.owned_households[i].residents.length;
+      
+      return count;
+    }
+    self.totalIncome = function(){
+      var count = 0;
+      for (i=0; i < dictator.owned_households.length; i++)
+        count+= dictator.owned_households[i].houshold_income;
+      
+      return count;
+    }
   }]);
 
   dictatorControllers.controller('modalCtrl', ['$scope', function($scope) {
